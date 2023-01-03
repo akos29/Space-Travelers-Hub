85e19c63feef25366dddcf669bf97190c0fcdb51
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-01-03 19:41:57 +0200

    Add rocket fetch and display with coresponding styling

--- src/components/DisplayRocket.js
+++ src/components/DisplayRocket.js
@@ -1,21 +1,22 @@
 /* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './DisplayRocket.module.css';
 
 function DisplayRocket({
   rocId, rocket_name, description, flickr_images,
 }) {
   return (
-    <>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.image}>
         <img src={flickr_images} alt={rocket_name} />
       </div>
-      <div>
+      <div className={styles.detail}>
         <h3>{rocket_name}</h3>
         <p>{description}</p>
         <button id={rocId} type="button">Reserve Rocket</button>
       </div>
-    </>
+    </div>
   );
 }
 
